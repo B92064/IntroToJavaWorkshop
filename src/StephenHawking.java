40762e00ddbded1e8e324f55f821eb4293ae6707
INeedAUniqueUsername <alexiscomical445@gmail.com>
2016-07-27 12:03:18 -0700

    ytsafa

--- src/StephenHawking.java
+++ src/StephenHawking.java
@@ -4,12 +4,12 @@ import javax.swing.JOptionPane;
 
 public class StephenHawking {
 public static void main(String[] args) {
-	JOptionPane.showInputDialog("Enter a Sentence");
+String A =	JOptionPane.showInputDialog("Enter a Sentence");
 
     // 1. make a main method and put steps 2, 3 & 4 inside it
     // 2. ask the user for a sentence
     // 3. call the speak method below and send it the sentence
-	speak("I like pizza");
+	speak(A);
     // 4. repeat steps 2 and 3 a lot of times
 }
 
